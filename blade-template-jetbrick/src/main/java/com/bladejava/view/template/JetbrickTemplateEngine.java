5f04de79c7e0b3d3a892eedc7d66ee77b347d5b0
biezhi <biezhi.me@gmail.com>
2016-02-29 13:20:02 +0800

    add jetbrick web loader

--- blade-template-jetbrick/src/main/java/com/bladejava/view/template/JetbrickTemplateEngine.java
+++ blade-template-jetbrick/src/main/java/com/bladejava/view/template/JetbrickTemplateEngine.java
@@ -1,8 +1,25 @@
+/**
+ * Copyright (c) 2016, biezhi 王爵 (biezhi.me@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bladejava.view.template;
 
 import java.io.Writer;
 import java.util.Map;
 import java.util.Set;
+
+import javax.servlet.ServletContext;
 
 import com.blade.context.BladeWebContext;
 import com.blade.view.template.ModelAndView;
@@ -14,13 +31,24 @@ import jetbrick.template.JetContext;
 import jetbrick.template.JetEngine;
 import jetbrick.template.JetTemplate;
 import jetbrick.template.TemplateException;
+import jetbrick.template.web.JetWebEngine;
 
+/**
+ * JetbrickTemplateEngine
+ * 
+ * @author	<a href="mailto:biezhi.me@gmail.com" target="_blank">biezhi</a>
+ * @since	1.0
+ */
 public class JetbrickTemplateEngine implements TemplateEngine {
 
 	private JetEngine jetEngine;
 
 	public JetbrickTemplateEngine() {
 		jetEngine = JetEngine.create();
+	}
+	
+	public JetbrickTemplateEngine(ServletContext servletContext) {
+		jetEngine = JetWebEngine.create(servletContext);
 	}
 
 	public JetbrickTemplateEngine(String conf) {
